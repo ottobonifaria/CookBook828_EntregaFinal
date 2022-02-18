1164d7e6b81422352bb59f1fb911d30deea94ac4
ottobonifaria <ottobonifaria.live.com>
2022-02-18 18:24:12 -0300

    Commit Inicial

---
+++ src/br/com/letscode/cookbook/view/EditReceitaView.java
@@ -0,0 +1,23 @@
+package br.com.letscode.cookbook.view;
+
+import br.com.letscode.cookbook.domain.Receita;
+
+public class EditReceitaView {
+
+    private Receita receita;
+
+    public EditReceitaView(Receita receita) {
+
+        this.receita = new Receita(receita);
+    }
+
+    public Receita edit() {
+        boolean confirm = false;
+        if (confirm) {
+
+            return receita;
+        } else {
+            return null;
+        }
+    }
+}
